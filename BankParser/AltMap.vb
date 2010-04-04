17bd890000a0c1b2c8109945cc9202b47d3a89f0
Austin Wise <AustinWise@gmail.com>
2010-04-04 04:20:19 -0700

    Made the alt file optional.

--- BankParser/AltMap.vb
+++ BankParser/AltMap.vb
@@ -7,13 +7,16 @@ Public Class AltMap
     ''' <summary>
     ''' Creates a new instance of the AltMap class.
     ''' </summary>
-    ''' <param name="altFile">The path to a file of alts.</param>
+    ''' <param name="altFile">The path to a file of alts.  If it does not exist, there will be no mapping of alts.</param>
     ''' <remarks>
     ''' The format of the file is one person per line.  On each line
     ''' there is the name of the person followed by a comma, followed
     ''' by a comma separated list of character names.
     ''' </remarks>
     Public Sub New(ByVal altFile As String)
+        If Not File.Exists(altFile) Then
+            Return
+        End If
         For Each line In File.ReadAllLines(altFile)
             Dim split As String() = line.Split(","c)
             For i As Integer = 1 To split.Length - 1
